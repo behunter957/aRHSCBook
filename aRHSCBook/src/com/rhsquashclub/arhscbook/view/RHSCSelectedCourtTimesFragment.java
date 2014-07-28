866016960c36e471d8bbc365a7dd27deabcad425
behunter957 <b_hunter@rogers.com>
2014-07-28 17:16:52 -0400

    fixed court time bug - removed list fragment class

--- aRHSCBook/src/com/rhsquashclub/arhscbook/view/RHSCSelectedCourtTimesFragment.java
+++ aRHSCBook/src/com/rhsquashclub/arhscbook/view/RHSCSelectedCourtTimesFragment.java
@@ -1,6 +1,8 @@
 package com.rhsquashclub.arhscbook.view;
 
 import com.rhsquashclub.arhscbook.R;
+import com.rhsquashclub.arhscbook.model.RHSCCourtTime;
+import com.rhsquashclub.arhscbook.model.RHSCSelectedCourtTimes;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,9 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RHSCSelectedCourtTimesFragment extends Fragment {
-	
+
+	private RHSCSelectedCourtTimes courts;
+
 	public RHSCSelectedCourtTimesFragment() {
 		// TODO Auto-generated constructor stub
 	}
@@ -25,12 +31,14 @@ public class RHSCSelectedCourtTimesFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle b) {
 		Log.i("RHSCSelectedCourtTimesFragment:onCreateView","entry");
 		View view = inflater.inflate(R.layout.fragment_selectedcourtlist, container, false);
-		FragmentManager fm = getFragmentManager();
-		  if (fm.findFragmentById(R.id.CourtListFragment) == null) {  
-			   RHSCSelectedCourtTimesListFragment list = new RHSCSelectedCourtTimesListFragment();  
-			   fm.beginTransaction().add(R.id.CourtListFragment, list).commit();  
-			  }  
+
+		getActivity().setTitle(R.string.courts_title); 
+		courts = RHSCSelectedCourtTimes.get(getActivity());
 		
+		ArrayAdapter<RHSCCourtTime> adapter = 
+				new ArrayAdapter<RHSCCourtTime>(getActivity(), android.R.layout.simple_list_item_1, courts);
+		ListView lv = (ListView) view.findViewById(R.id.CourtListFragment);
+		lv.setAdapter( adapter);		
 		
 		// use view.findViewById(id) to set values in the view
 		
